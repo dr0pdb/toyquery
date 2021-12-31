b3b8b802fda6552d7531350168e16639593e2745
Saurav Tiwary <srv.twry@gmail.com>
2021-12-31 19:56:34 +0530

    use stod for now
    Signed-off-by: Saurav Tiwary <srv.twry@gmail.com>

--- include/common/utils.h
+++ include/common/utils.h
@@ -24,16 +24,17 @@ absl::StatusOr<long> ToLong(const absl::string_view& input) {
 }
 
 absl::StatusOr<double> ToDouble(const absl::string_view& input) {
-  double out;
-  const std::from_chars_result result = std::from_chars(input.data(), input.data() + input.size(), out);
-  if (result.ec == std::errc::invalid_argument) {
-    return absl::InvalidArgumentError(absl::StrCat(input, " is an invalid long"));
-  }
-  if (result.ec == std::errc::result_out_of_range) {
-    return absl::OutOfRangeError(absl::StrCat(input, " is out of range for long"));
-  }
+  // double out;
+  // const std::from_chars_result result = std::from_chars(input.data(), input.data() + input.size(), out);
+  // if (result.ec == std::errc::invalid_argument) {
+  //   return absl::InvalidArgumentError(absl::StrCat(input, " is an invalid long"));
+  // }
+  // if (result.ec == std::errc::result_out_of_range) {
+  //   return absl::OutOfRangeError(absl::StrCat(input, " is out of range for long"));
+  // }
 
-  return out;
+  // return out;
+  return std::stod(std::string(input));
 }
 
 }  // namespace common
